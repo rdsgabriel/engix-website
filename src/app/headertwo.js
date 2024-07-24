5b94f344fad97ac9cbc4b8b1df3fa3285df754e7
rdsgabriel <gabriel.rodrigues.ismart@gmail.com>
2024-07-24 18:11:19 -0300

    font-swap

--- src/app/headertwo.js
+++ src/app/headertwo.js
@@ -2,7 +2,7 @@ export default function HeaderTwo() {
   return (
    <div className="bg-azul1 gap-6 py-6 justify-center hidden sm:flex font-sf-display text-gray-200 text-sm tracking-wide border-b border-azul3">
     <a href="" className="hover:text-gray-400">Desenvolvimento</a>
-    <a href="" className="hover:text-gray-400">Marketing</a>
+    <a href="" className="hover:text-gray-400">Data Science</a>
     <a href="" className="hover:text-gray-400">Design</a>
     <a href="" className="hover:text-gray-400">Sobre Nós</a>
     <a href="" className="hover:text-gray-400">Clientes</a>
