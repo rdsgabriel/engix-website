dbd180ac3d3157f794fe74a02741796220311090
rdsgabriel <gabriel.rodrigues.ismart@gmail.com>
2024-08-02 16:44:50 -0300

    bg-white

--- src/app/presentation.js
+++ src/app/presentation.js
@@ -1,6 +1,6 @@
 export default function Presentation() {
   return (
-    <div className="bg-branco sm:rounded-xl sm:-mt-24 min-h-full mx-auto sm:min-h-96 min-w-screen md:mx-auto md:max-w-2xl lg:mx-auto 2xl:mx-auto xs:mx-40 sm:max-w-xl sm:mx-auto lg:max-w-4xl 2xl:max-w-7xl flex justify-center items-center text-azul3 sm:mb-14">
+    <div className="bg-transparent sm:rounded-xl sm:-mt-24 min-h-full mx-auto sm:min-h-96 min-w-screen md:mx-auto md:max-w-2xl lg:mx-auto 2xl:mx-auto xs:mx-40 sm:max-w-xl sm:mx-auto lg:max-w-4xl 2xl:max-w-7xl flex justify-center items-center text-azul3 sm:mb-14">
       <div className="relative">
         <video 
           src="/images/compo.webm" 
